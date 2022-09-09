248663ca30736816d1b0f4e248703ddd2dde441c
SHEF <Shefqetmush@gmail.com>
2022-09-09 13:30:42 -0400

    password validity work

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let password = document.querySelector("#password");
 let confirm = document.querySelector("#confirm");
 let button = document.querySelector("button");
+let errormessage = document.querySelector(".error");
 
 let passwordvalue = "";
 let confirmvalue = "";
@@ -19,9 +20,12 @@ button.addEventListener("click", () => thesame());
 
 function thesame() {
   if (passwordvalue != "" && passwordvalue === confirmvalue) {
-    console.log("True!");
+    password.setCustomValidity("");
+    confirm.setCustomValidity("");
+    errormessage.setAttribute("hidden", true);
   } else if (passwordvalue != "" && passwordvalue != confirmvalue) {
     password.setCustomValidity("Invalid field.");
     confirm.setCustomValidity("Invalid field.");
+    errormessage.removeAttribute("hidden");
   }
 }
